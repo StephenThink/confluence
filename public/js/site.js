745cd36685b382e63c411916ad150348c99292a8
StephenThink <86749311+StephenThink@users.noreply.github.com>
2022-05-31 14:44:54 +0100

    Nav Bar 90 percent

--- public/js/site.js
+++ public/js/site.js
@@ -10,8 +10,12 @@
 // This is all you.
 var ham = document.getElementById('ham');
 var navmenu = document.getElementById('navmenu');
+var burger = document.getElementById('burger');
+var close = document.getElementById('close');
 ham.addEventListener("click", function () {
   navmenu.classList.toggle("open");
+  burger.classList.toggle("hidden");
+  close.classList.toggle("hidden");
 });
 
 /***/ }),
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,7 +1,12 @@
 // This is all you.
 const ham = document.getElementById('ham');
 const navmenu = document.getElementById('navmenu');
+const burger = document.getElementById('burger');
+const close = document.getElementById('close');
 
 ham.addEventListener("click", () => {
     navmenu.classList.toggle("open");
+    burger.classList.toggle("hidden");
+    close.classList.toggle("hidden");
+
 })
