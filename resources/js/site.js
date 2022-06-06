a91e5e836d4c8b6683deaf8543127fcc804e3ced
StephenThink <86749311+StephenThink@users.noreply.github.com>
2022-06-06 16:18:38 +0100

    Hero is a Flickity Carousel

--- resources/js/site.js
+++ resources/js/site.js
@@ -19,69 +19,87 @@ Alpine.start()
 //
 // })
 
-const hero = document.getElementById('heroContainer');
-const cell = document.querySelectorAll('cell');
-const dot = document.querySelectorAll('.dot');
-const cellTotal = cell.length;
-let index = 0;
-let prevIndex = 0;
+// const hero = document.getElementById('heroContainer');
+// const cell = document.querySelectorAll('cell');
+// const dot = document.querySelectorAll('.dot');
+// const cellTotal = cell.length;
+// let index = 0;
+// let prevIndex = 0;
+//
+// if (hero) {
+//     initCells();
+//     dotAction();
+//
+//     // nextCell();
+// }
+//
+// function initCells() {
+//     cell[index].classList.remove('translate-x-full')
+//     cell[index].classList.toggle('hidden');
+//     dot[index].classList.add('dot-active');
+// }
+//
+// function clickDot(e) {
+//     console.log(e);
+// }
+//
+// function dotAction() {
+//     dot.forEach(function(dots) {
+//         let dotIndex = dots.getAttribute('data-index');
+//
+//         dots.addEventListener('click', function(e) {
+//             console.log(dotIndex);
+//             prevIndex = index;
+//             index = dotIndex;
+//             dotClearing();
+// cellClearing();
+//             // Clicking the Dot turns it active.
+//             dot[index].classList.add('dot-active');
+//         });
+//     })
+// }
+//
+// function dotClearing() {
+//     dot.forEach((item, index) => {
+//         if (item.classList.contains('dot-active')) {
+//             item.classList.remove('dot-active');
+//         }
+//     })
+// }
+//
+// function cellClearing() {
+//
+//     cell[index].classList.add('translate-x-full')
+//     cell[index].classList.remove('hidden')
+//     cell[index].classList.remove('translate-x-full')
+//
+//     // cell[prevIndex].classList.add('slideOutLeft')
+//     // cell[index].classList.remove('fadeIn')
+//    // cell[index].classList.add('fadeIn')
+//     // Animate and Hide the Previous Cell
+//     // setTimeout(() => {
+//     //     cell[prevIndex].classList.remove('slideInLeft')
+//     // }, 2000);
+//     setTimeout(() => {
+//         cell[prevIndex].classList.add('hidden')
+//     }, 2000);
+// }
+import Flickity from 'flickity-fade';
 
-if (hero) {
-    initCells();
-    dotAction();
+let el = document.querySelector('.main-carousel');
 
-    // nextCell();
-}
+if( el ) {
 
-function initCells() {
-    cell[index].classList.remove('translate-x-full')
-    cell[index].classList.toggle('hidden');
-    dot[index].classList.add('dot-active');
-}
-
-function clickDot(e) {
-    console.log(e);
-}
-
-function dotAction() {
-    dot.forEach(function(dots) {
-        let dotIndex = dots.getAttribute('data-index');
-
-        dots.addEventListener('click', function(e) {
-            console.log(dotIndex);
-            prevIndex = index;
-            index = dotIndex;
-            dotClearing();
-cellClearing();
-            // Clicking the Dot turns it active.
-            dot[index].classList.add('dot-active');
-        });
+    let flkty = new Flickity( el, {
+        // options
+        pageDots: true,
+        prevNextButtons: false,
+        fade: true,
+        draggable: false,
+        autoPlay: true,
+        autoPlay: 10000,
+        pauseAutoPlayOnHover: false,
+        lazyLoad: true,
     })
+
 }
-
-function dotClearing() {
-    dot.forEach((item, index) => {
-        if (item.classList.contains('dot-active')) {
-            item.classList.remove('dot-active');
-        }
-    })
-}
-
-function cellClearing() {
-
-    cell[index].classList.add('translate-x-full')
-    cell[index].classList.remove('hidden')
-    cell[index].classList.remove('translate-x-full')
-
-    // cell[prevIndex].classList.add('slideOutLeft')
-    // cell[index].classList.remove('fadeIn')
-   // cell[index].classList.add('fadeIn')
-    // Animate and Hide the Previous Cell
-    // setTimeout(() => {
-    //     cell[prevIndex].classList.remove('slideInLeft')
-    // }, 2000);
-    setTimeout(() => {
-        cell[prevIndex].classList.add('hidden')
-    }, 2000);
-}
-
